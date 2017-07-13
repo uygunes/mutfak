be9db760a62e7e1c3ad8fc0a1b70bc440daa3005
Umit Yilmaz Gunes <uygunes@rbtx.org>
2017-07-13 15:25:08 +0300

    main page

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+
+
+	def index
+		render 'layouts/index'
+	end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "home#index"
   resources :controllers
   resources :birims
   resources :malzemes
